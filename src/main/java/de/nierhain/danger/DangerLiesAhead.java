d06f3f8ec997c65302faea48421385610a8df6c0
Nierhain <robin@nierhain.de>
2018-06-30 02:06:00 +0200

    intial commit

---
+++ src/main/java/de/nierhain/danger/DangerLiesAhead.java
@@ -0,0 +1,9 @@
+package de.nierhain.danger;
+
+import de.nierhain.danger.utils.Reference;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(modid = Reference.MODID, name = Reference.NAME, version = Reference.VERSION)
+public class DangerLiesAhead {
+
+}
---
+++ src/main/java/de/nierhain/danger/utils/Reference.java
@@ -0,0 +1,8 @@
+package de.nierhain.danger.utils;
+
+public class Reference {
+    public static final String MODID = "dla";
+    public static final String NAME = "Danger Lies Ahead";
+    public static final String VERSION = "v0.1";
+
+}
