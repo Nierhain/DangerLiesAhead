caba51efe8a1bb93e3fbba2d5145672a42605ef9
Nierhain <robin@nierhain.de>
2018-07-04 13:31:41 +0200

    fixed keybinds

--- src/main/java/de/nierhain/danger/handler/KeyHandler.java
+++ src/main/java/de/nierhain/danger/handler/KeyHandler.java
@@ -2,25 +2,33 @@ package de.nierhain.danger.handler;
 
 
 import de.nierhain.danger.gui.GuiSkill;
-import de.nierhain.danger.proxy.ClientProxy;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.settings.KeyBinding;
-import net.minecraftforge.fml.common.eventhandler.EventPriority;
+import net.minecraftforge.fml.client.registry.ClientRegistry;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import net.minecraftforge.fml.common.gameevent.InputEvent;
-import net.minecraftforge.fml.relauncher.Side;
-import net.minecraftforge.fml.relauncher.SideOnly;
+import net.minecraftforge.fml.common.gameevent.InputEvent.KeyInputEvent;
+import org.lwjgl.input.Keyboard;
 
 public class KeyHandler {
 
-    @SideOnly(Side.CLIENT)
-    @SubscribeEvent(priority = EventPriority.NORMAL, receiveCanceled = true)
-    public static void onKeyInput(InputEvent.KeyInputEvent event){
-        KeyBinding[] binds = ClientProxy.getBinds();
+    private final Minecraft mc;
 
-        if(binds[0].isPressed()){
-            System.out.println("Key pressed");
-            Minecraft.getMinecraft().displayGuiScreen(new GuiSkill());
+    public static final KeyBinding[] binds = new KeyBinding[1];
+
+    public KeyHandler(Minecraft mc){
+        this.mc = mc;
+
+        binds[0] = new KeyBinding("key.gui.desc", Keyboard.KEY_X, "key.danger.category");
+        ClientRegistry.registerKeyBinding(binds[0]);
+    }
+
+    @SubscribeEvent()
+    public void onKeyInput(KeyInputEvent event){
+
+        if(mc.inGameHasFocus){
+            if(binds[0].isPressed()){
+                mc.displayGuiScreen(new GuiSkill());
+            }
         }
     }
 }
