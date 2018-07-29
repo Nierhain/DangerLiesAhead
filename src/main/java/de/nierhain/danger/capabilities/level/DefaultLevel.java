94e64813a65c91b791a8432d0f756f2bd2f780cb
Robin Meischke <robin@nierhain.de>
2018-07-30 00:17:51 +0200

    removed skillpoints from level cap
    it seems that past me's logic wasn't on point

--- src/main/java/de/nierhain/danger/capabilities/level/DefaultLevel.java
+++ src/main/java/de/nierhain/danger/capabilities/level/DefaultLevel.java
@@ -5,8 +5,6 @@ public class DefaultLevel implements ILevel {
 
     private int level = 0;
     private int xp = 0;
-    private int skillpointsAvailable = 0;
-    private int skillpointsMax = 0;
 
     @Override
     public int getLevel() {
@@ -18,34 +16,15 @@ public class DefaultLevel implements ILevel {
         return this.xp;
     }
 
-    @Override
-    public int getSkillpointsAvailable() {
-        return this.skillpointsAvailable;
-    }
-
-    @Override
-    public int getSkillpointsMax() {
-        return this.skillpointsMax;
-    }
 
     @Override
     public void setLevel(int amount) {
-        this.skillpointsAvailable = amount;
+        this.level = amount;
     }
 
     @Override
     public void setXP(int amount) {
         this.xp = amount;
-    }
-
-    @Override
-    public void setSkillpointsAvailable(int amount) {
-        this.skillpointsAvailable = amount;
-    }
-
-    @Override
-    public void setSkillpointsMax(int amount) {
-        this.skillpointsMax = amount;
     }
 
     @Override
@@ -59,29 +38,8 @@ public class DefaultLevel implements ILevel {
     }
 
     @Override
-    public void addSkillpointsAvailable(int amount) {
-        this.skillpointsAvailable += amount;
-    }
-
-    @Override
-    public void addSkillpointsMax(int amount) {
-        this.skillpointsMax += amount;
-    }
-
-    @Override
-    public void removeSkillpointsAvailable(int amount) {
-        this.skillpointsAvailable -= amount;
-    }
-
-    public boolean hasSkillpointsAvailable() {
-        return skillpointsAvailable > 0;
-    }
-
-    @Override
     public void reset(){
         level = 0;
         xp = 0;
-        skillpointsAvailable = 0;
-        skillpointsMax = 0;
     }
 }
--- src/main/java/de/nierhain/danger/capabilities/level/ILevel.java
+++ src/main/java/de/nierhain/danger/capabilities/level/ILevel.java
@@ -10,15 +10,6 @@ public interface ILevel {
     public void setXP(int amount);
     public void addXP(int amount);
 
-    public int getSkillpointsAvailable();
-    public void setSkillpointsAvailable(int amount);
-    public void addSkillpointsAvailable(int amount);
-    public void removeSkillpointsAvailable(int amount);
-
-    public int getSkillpointsMax();
-    public void setSkillpointsMax(int amount);
-    public void addSkillpointsMax(int amount);
-
     public void reset();
 
 
